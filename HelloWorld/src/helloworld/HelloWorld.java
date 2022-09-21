d682ba5813b6428d2a428cb50d18bf75ae9de0e8
asosa <asosa@106-30.info2>
2022-09-21 13:04:58 +0200

    Hello World

---
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -0,0 +1,12 @@
+package helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola mundo");
+		int a=3, b=5, c=a+b;
+		System.out.println("La suma es "+c);
+		
+	}
+}
